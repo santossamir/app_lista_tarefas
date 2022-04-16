68eae324fdda66e8da149f2985eba9ac5a5d8db6
Samir Santos <santossamir@hotmail.com>
2022-04-16 18:16:42 -0300

    Recuperando as informações do banco de dados e apresentando na pag Todas as tarefas.

--- app_protegido/tarefa.service.php
+++ app_protegido/tarefa.service.php
@@ -16,12 +16,25 @@
             $stmt->bindValue(':tarefa', $this->tarefa->__get('tarefa'));
             $stmt->execute();
         }
+
         public function recuperar(){
+            $query_consultar = '
+                select 
+                    t.id, s.status, t.tarefa
+                from
+                    tb_tarefas as t
+                    left join tb_status as s on (t.id_status = s.id)
+            ';
+            $stmt = $this->conexao->prepare($query_consultar);
+            $stmt->execute();
             
+            return $stmt->fetchAll(PDO::FETCH_OBJ);
         }
+
         public function atualizar(){
             
         }
+
         public function remover(){
             
         }
--- app_protegido/tarefa_controller.php
+++ app_protegido/tarefa_controller.php
@@ -4,13 +4,25 @@
    require "./app_protegido/tarefa.service.php";
    require "./app_protegido/conexao.php";
 
-   $tarefa = new Tarefa();
-   $tarefa->__set('tarefa', $_POST['tarefa']);
+   $acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
-   $conexao = new Conexao();
+   if($acao == 'inserir'){
+      $tarefa = new Tarefa();
+      $tarefa->__set('tarefa', $_POST['tarefa']);
 
-   $tarefaService = new TarefaService($conexao, $tarefa);
-   $tarefaService->inserir();
+      $conexao = new Conexao();
 
-   header('Location: nova_tarefa.php?inclusao=1');
+      $tarefaService = new TarefaService($conexao, $tarefa);
+      $tarefaService->inserir();
+
+      header('Location: nova_tarefa.php?inclusao=1');
+
+   }else if($acao == 'recuperar'){
+
+      $tarefa = new Tarefa();
+      $conexao = new Conexao();
+
+      $tarefaService = new TarefaService($conexao, $tarefa);
+      $tarefas = $tarefaService->recuperar();
+   }
 ?>
