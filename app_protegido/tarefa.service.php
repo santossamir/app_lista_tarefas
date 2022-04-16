9b6e05250e4c9c119f9d7a8b5862b45ee247da10
Samir Santos <santossamir@hotmail.com>
2022-04-16 15:17:01 -0300

    Criando a class TarefaService, que é o CRUD do nosso app.

---
+++ app_protegido/tarefa.service.php
@@ -0,0 +1,18 @@
+<?php
+
+    #CRUD do app
+    class TarefaService{
+        public function inserir(){
+
+        }
+        public function recuperar(){
+            
+        }
+        public function atualizar(){
+            
+        }
+        public function remover(){
+            
+        }
+    }
+?>
