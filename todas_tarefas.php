29c2cd9b5bb2f59b1f28d1c1e74447a6b9543a53
Samir Santos <santossamir@hotmail.com>
2022-04-20 13:53:50 -0300

    Desenvolvendo o editar da page todas as tarefas.

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -11,6 +11,53 @@
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+		
+		<script>
+			function editar(id, txt_tarefa){
+				//Form de Edicação
+				let form = document.createElement('form');
+				form.action = '#';
+				form.method = 'post';
+				form.className = 'row';
+
+				//Input para entrada de texto
+				let inputTarefa = document.createElement('input');
+				inputTarefa.type = 'text';
+				inputTarefa.name = 'tarefa';
+				inputTarefa.className = 'col-9 form-control';
+				inputTarefa.value = txt_tarefa; 
+
+				//Criar input hidden para guardar o id da tarefa
+				let inputId = document.createElement('input');
+				inputId.type = 'hidden';
+				inputId.name = 'id';
+				inputId.value = id;
+				
+				//Button para envio do form
+				let button = document.createElement('button');
+				button.type = 'submit';
+				button.className = 'col-3 btn btn-info';
+				button.innerHTML = 'Atualizar';
+
+				//Incluir inputTarefa no form
+				form.appendChild(inputTarefa);
+
+				//Incluir inputId no form
+				form.appendChild(inputId);
+
+				//Incluir inputTarefa no form
+				form.appendChild(button);
+
+				//Selecionando a div tarefa
+				let tarefa = document.getElementById('tarefa_'+id);
+
+				//Limpar texto da tarefa para incluir o form
+				tarefa.innerHTML = '';
+
+				//Incluir form na pag
+				tarefa.insertBefore(form, tarefa[0]);
+			}
+		</script>
 	</head>
 
 	<body>
@@ -43,10 +90,10 @@
 									foreach($tarefas as $dado => $tarefa){ 
 								?>
 									<div class="row mb-3 d-flex align-items-center tarefa">
-										<div class="col-sm-9"><?php echo $tarefa->tarefa ?> (<?php echo $tarefa->status?>)</div>
+										<div class="col-sm-9" id="tarefa_<?php echo $tarefa->id ?>"><?php echo $tarefa->tarefa ?> (<?php echo $tarefa->status?>)</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger"></i>
-											<i class="fas fa-edit fa-lg text-info"></i>
+											<i class="fas fa-edit fa-lg text-info" onclick="editar(<?php echo $tarefa->id?>, '<?php echo $tarefa->tarefa ?>')"></i>
 											<i class="fas fa-check-square fa-lg text-success"></i>
 										</div>
 									</div>
